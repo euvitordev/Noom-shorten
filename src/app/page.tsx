6f60cc5fff47dc723e1bae92c9848dd87a140f25
Vitor Valente <dev.vitorlucas@gmail.com>
2025-01-19 13:16:25 -0300

    feat: add shadcn library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header className=""></header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className="flex items-center justify-center gap-4">
+          <h1 className="font-bold text-9xl">Noom</h1>
+          <span className="w-6 h-6 bg-black rounded-full animate-pulse" />
+        </div>
+        <div className="flex items-center justify-center ml-2 gap-2">
+          <span className="w-1 h-4 bg-black rounded-full" />
+          <p className="">Simplicidade nos seus links com apenas um clique.</p>
+        </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -37,16 +29,16 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Converse com o fundador
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <Link
+            className="rounded-full border border-solid gap-4 group border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="/dashboard"
           >
-            Read our docs
-          </a>
+            Conheça o Projeto
+
+            {/* <MoveRight size={18} className="group-hover:translate-x-2 transition-transform delay-75 duration-300" /> */}
+          </Link>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
